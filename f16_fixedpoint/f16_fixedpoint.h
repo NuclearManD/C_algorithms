fcfb81c5c5b9c80174d4d6e0599f5922591da33b
Dylan Brophy <dbrophy@student.42.us.org>
2019-11-03 12:13:29 -0800

    added E and PI constants (see header for details)

--- f16_fixedpoint/f16_fixedpoint.h
+++ f16_fixedpoint/f16_fixedpoint.h
@@ -3,6 +3,9 @@
 # define FP_TO_FLOAT(x) (x)/65536.0f
 # define FLOAT_TO_FP(x) (int)((x)*65536)
 
+# define FP_PI	205887
+# define FP_E	178145
+
 int mul_fp(int a, int b);
 int div_fp(int a, int b);
 
